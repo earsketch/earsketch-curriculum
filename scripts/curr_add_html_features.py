17cdf0d14b089e059bc4586df55772955aaf6a20
Stephen Garrett <garrett@gatech.edu>
2020-12-04 15:30:35 -0500

    initial build scripts

---
+++ scripts/curr_add_html_features.py
@@ -0,0 +1,121 @@
+# Parse curriculum html files and perform changes to the text
+
+import os, os.path
+import codecs
+from bs4 import BeautifulSoup
+import subprocess
+import re
+import sys
+
+if len(sys.argv) < 2:
+	print("Error, no arguments given")
+	print("Usage: curr_add_html_features.py <GIT_REPO_DIR>")
+	exit(1)
+curr_dir = sys.argv[1]
+
+slides_dir = curr_dir+'/teacher-materials/'
+slides_output_dir = curr_dir+'/curriculum-asciidoc/teachermaterials/'
+jar_file = curr_dir+'/utilities/pptx_convertor.jar'
+video_caption_dir = curr_dir+'/webclient/videoMedia/NewCaptions/'
+curr_dir = curr_dir+'/webclient/curriculum/'
+
+caption_files = [cap_name[:-4] for cap_name in os.listdir(video_caption_dir)]
+
+chapters = [ch for ch in os.listdir(curr_dir) if 'ch_' in ch]
+units = [unit for unit in os.listdir(slides_dir) if 'Unit' in unit]
+
+print("Processing html files...")
+n_processed = 0
+for ch in chapters:
+	n_processed += 1
+
+	data = codecs.open(curr_dir + ch, 'r').read()
+	soup = BeautifulSoup(data, 'html.parser')
+
+	# fix the image file paths
+	for el in soup.find_all('img', src=lambda x: x.startswith('../media')):
+		el['src'] = el['src'].replace('../media', 'curriculum/media')
+
+	# fix special video paths
+	for el in soup.find_all('video', src=lambda x: x.startswith('../media')):
+		el['src'] = el['src'].replace('../media', 'curriculum/media')
+
+	# remove the unwanted inline style chunk
+	[el.extract() for el in soup('style')]
+
+	# replace the divs with "mp3" contents with an audio tag
+	for el in soup.find_all('div', {'class':'curriculum-mp3'}):
+		el.contents[0] = soup.new_tag('audio', src=el.string, controls='')
+
+	# for highlight.js to work
+	for el in soup.find_all('code', {'class': lambda x: x and x.startswith('language-')}):
+		el['class'] = el['class'][0].replace('language-', '')
+
+	# add the code-paste button
+	for el in soup.find_all('pre', {'class':'highlight'}):
+		lang = el('code')[0]['class']
+		skip = False
+		if lang == 'python':
+			lang_str = 'python'
+		elif lang == 'javascript': 
+			lang_str = 'javascript'
+		else:
+			skip = True #don't add copy icon
+
+		container = soup.new_tag('div')
+		container['class'] = 'currcode-container'
+		el.wrap(container)
+
+		if not skip:
+			button = soup.new_tag('button')
+			button['class'] = 'btn-copy copy-btn-' + lang_str
+			button['onclick'] = 'doCopy(this)'
+			button['style'] = 'display:block'
+
+			i = soup.new_tag('i')
+			i['class'] = 'icon icon-paste2'
+			i['title'] = 'Open the example code in the editor'
+	
+			el.insert_before(i)
+			i.wrap(button)
+
+	# # add target="_blank" to outbound links
+	# for el in soup.find_all('a', href=lambda x: x.startswith('http')):
+	# 	el['target'] = '_blank'
+
+	# open internal cross references with loadChapter()
+	# TODO: figure out ng-binding with the curriculumPaneController
+	# for x-refs pointing to a different section in the same chapter
+	for el in soup.find_all('a', href=lambda x: x.startswith('#')):
+		el['onclick'] = 'angular.element(\'[ng-controller=layoutController]\').scope().loadChapter(\'' + ch + el['href'] + '\')'
+		el['href'] = '#'
+
+	for el in soup.find_all('a', href=lambda x: x.startswith('ch_')):
+		el['onclick'] = 'angular.element(\'[ng-controller=layoutController]\').scope().loadChapter(\'' + el['href'] + '\')'
+		el['href'] = '#'
+
+	# fix the link to open the ES API browser
+	for el in soup.find_all('a', href='<toc.html>'):
+		el['href'] = '#'
+		el['onclick'] = 'angular.element(\'[ng-controller=layoutController]\').scope().openSidebarTab(\'api\')'
+
+	for el in soup('video'):
+		# If a caption file with matching name is found, add it to HTML.
+		# Note: The names of video file and caption file have to be identical!
+		video_name = el['src'][13:-4]
+		if video_name in caption_files:
+			track_tag = soup.new_tag('track', src='./videoMedia/NewCaptions/'+video_name+'.vtt', srclang='en', kind='captions')
+			# track_tag.attrs['default'] = None # Comment in if we want to show the captions by default.
+			el.append(track_tag)
+		el['preload'] = 'none'
+		el['poster'] = 'img/video-thumbnail.png'
+
+	#special case for teacher materials link
+	for el in soup.find_all('a', href=lambda x: x.startswith('teachermaterials')):
+		el['onclick'] = 'downloadProtectedData()'
+		el['href'] = '#'
+
+	wf = open(curr_dir + ch, 'wb')
+	wf.write(soup.encode_contents())
+	wf.close()
+print(str(n_processed) + " html files processed")
---
+++ scripts/curr_toc.py
@@ -0,0 +1,112 @@
+# Parses curriculum html files to create table-of-contents JSON data structure
+
+import codecs, os
+import json
+from bs4 import BeautifulSoup
+import sys
+
+if len(sys.argv) < 2:
+	print("Error, no arguments given")
+	print("Usage: curr_toc.py <GIT_REPO_DIR>")
+	exit(1)
+curr_dir = sys.argv[1]
+
+toc = codecs.open(curr_dir+'/webclient/curriculum/toc.html', 'r').read()
+parser = BeautifulSoup(toc, 'html.parser')
+toc_data = []
+chapter_count = 0
+display_chapter_number = -1
+
+# units
+print("Processing html files to create Table-Of-Contents data structure...")
+n_processed_units = 0
+n_processed_ch = 0
+for unit in parser.find_all('div', attrs={'class':'sect1'}):
+	n_processed_units += 1
+	
+	unit_data = {
+		'title': unit.find('a').text,
+		'URL': unit.find('a').attrs['href'],
+		'chapters': [],
+		'sections': []
+	}
+
+	# # parse and add sections for the units with no chapters
+	# url = unit.find('a').attrs['href']
+	# unit_html = codecs.open(curr_dir+'/webclient/curriculum/'+url, 'r').read()
+	# sections = BeautifulSoup(unit_html, 'html.parser')
+	#
+	# for section in sections.find_all('div', attrs={'class':'sect2'}):
+	# 	sec_data = {
+	# 		'title': section.find('h3').text,
+	# 		'URL': url+'#'+section.find('h3').attrs['id'],
+	# 	}
+	# 	unit_data['sections'].append(sec_data)
+
+	# chapters
+	for chapter in unit.find_all('div', attrs={'class':'sect2'}):
+		n_processed_ch += 1
+
+		url = chapter.find('a').attrs['href']
+
+		# check if Intro section is present in this unit
+		if url.find("_intro") < 0 and url.find("_summary") < 0:
+			chapter_count = chapter_count + 1
+			display_chapter_number = chapter_count
+		else:
+			display_chapter_number = -1
+
+		# read the html of current chapter
+		chapter_html = codecs.open(curr_dir+'/webclient/curriculum/'+url, 'r').read()
+		sections = BeautifulSoup(chapter_html, 'html.parser')
+
+		ch_data = {
+			'title': chapter.find('a').text,
+			'URL': url,
+			'displayChNum': display_chapter_number,
+			'sections': []
+		}
+
+		# subsections of chapter
+		for section in sections.find_all('div', attrs={'class':'sect2'}):
+			sec_data = {
+				'title': section.find('h3').text,
+				'URL': url+'#'+section.find('h3').attrs['id'],
+			}
+
+			ch_data['sections'].append(sec_data)
+
+		unit_data['chapters'].append(ch_data)
+
+	toc_data.append(unit_data)
+
+toc_pages = []
+
+for unitIdx, unit in enumerate(toc_data):
+	if len(unit['chapters']) == 0:
+		toc_pages.append([unitIdx])
+
+	for chIdx, ch in enumerate(unit['chapters']):
+		if len(ch['sections']) == 0:
+			toc_pages.append([unitIdx, chIdx])
+		for secIdx, sec in enumerate(ch['sections']):
+			toc_pages.append([unitIdx, chIdx, secIdx])
+
+wf = open(curr_dir+'/webclient/scripts/src/data/curr_toc.js', 'w')
+wf.write('var ESCurr_TOC = ' + json.dumps(toc_data, indent=4) + ';')
+wf.close()
+
+wf = open(curr_dir+'/curriculum-asciidoc/curr_toc.js', 'w')
+wf.write('var ESCurr_TOC = ' + json.dumps(toc_data, indent=4) + ';')
+wf.close()
+
+wf = open(curr_dir+'/webclient/scripts/src/data/curr_pages.js', 'w')
+wf.write('var ESCurr_Pages = ' + json.dumps(toc_pages) + ';')
+wf.close()
+
+wf = open(curr_dir+'/curriculum-asciidoc/curr_pages.js', 'w')
+wf.write('var ESCurr_Pages = ' + json.dumps(toc_pages) + ';')
+wf.close()
+
+print(str(n_processed_units) + " units with " + str(n_processed_ch) + " chapters processed")
+
