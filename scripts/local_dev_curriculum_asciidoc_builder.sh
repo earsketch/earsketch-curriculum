d3be17f5b3ed9a319c3d0c9cf4c90458bdd64d9c
Stephen Garrett <garrett@gatech.edu>
2021-03-25 13:58:46 -0400

    revert local build script back

--- scripts/local_dev_curriculum_asciidoc_builder.sh
+++ scripts/local_dev_curriculum_asciidoc_builder.sh
@@ -16,7 +16,7 @@ fi
 
 ASCIIDOC_DIR=$GIT_REPO/src
 LOCAL_STAGING_DIR=$GIT_REPO/curriculum-local/
-ES_HOST="https://earsketch-test.ersktch.gatech.edu"
+ES_HOST="http://localhost:8080"
 
 cd "$GIT_REPO" || exit 1
 echo "Converting curriculum to html with asciidoctor..."
